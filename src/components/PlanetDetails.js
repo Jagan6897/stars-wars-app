4e4320f31dbd03a3947494b80b371f77aac9d239
Jagan6897 <jaganreddy6897@gmail.com>
2024-09-16 12:11:29 -0500

    Update PlanetDetails.js

--- src/components/PlanetDetails.js
+++ src/components/PlanetDetails.js
@@ -1,4 +1,3 @@
-// src/components/PlanetDetails.js
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { getResidents } from '../api/starWarsAPI';
